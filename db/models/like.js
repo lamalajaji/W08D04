ef88513f499aedab95534b197ac2db18beb9eea0
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-01 12:19:42 +0300

    create likes schema

--- db/models/like.js
+++ db/models/like.js
@@ -1,1 +1,23 @@
 const mongoose = require("mongoose");
+
+
+
+//// likes schema
+const likeSchema = new mongoose.Schema({
+  isLike: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  post:  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post" }
+  
+});
+
+const likeSchema = mongoose.model("Like", likeSchema);
+
+module.exports = likeSchema;
--- db/models/post.js
+++ db/models/post.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
 const postSchema = new mongoose.Schema({
   desc: {
     type: String,
