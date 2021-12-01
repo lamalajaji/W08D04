b8e64c5042b059e77856a8020e371181750479aa
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-01 13:03:04 +0300

    create comment schema

--- db/models/comment.js
+++ db/models/comment.js
@@ -1,1 +1,22 @@
 const mongoose = require("mongoose");
+
+//// likes schema
+const commentSchema = new mongoose.Schema({
+  comment: { type: String , required : true},
+    isDel: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+});
+
+const commentSchema = mongoose.model("Comment", commentSchema);
+
+module.exports = commentSchema;
