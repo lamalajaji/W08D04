8a67f537034ec13d815a2a5062c12f141fced2d4
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-01 12:14:35 +0300

    create post schema

--- db/models/post.js
+++ db/models/post.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+
+
+const postSchema = new mongoose.Schema({
+  desc: {
+    type: String,
+    required: true,
+  },
+  title: { type: String },
+  isDel: {
+    type: Boolean,
+    default: false,
+  },
+  img: {
+    type: String,
+  },
+  createdAt: { type: Date, default: Date.now },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  likes: { type: mongoose.Schema.Types.ObjectId, ref: "Like" },
+});
+
+const postSchema = mongoose.model("Post", postSchema);
+
+module.exports = postSchema;
